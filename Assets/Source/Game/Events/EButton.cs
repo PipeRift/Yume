5ebc5d83de1fdfd651ea87ee9c08269aa3cba3f2
muit <miguel_3c@hotmail.com>
2016-03-28 16:58:56 +0200

    Stairs & Cubes

---
+++ Assets/Source/Game/Events/EButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class EButton : Crab.Event {
+    public enum ColorType {
+        Red,
+        Green,
+        Blue
+    }
+    public ColorType color;
+
+    private new Renderer renderer;
+
+    protected override void OnGameStart(SceneScript scene)
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update() {
+        if (!Application.isPlaying) {
+            UpdateColor();
+        }
+    }
+
+
+    void UpdateColor() {
+        switch (color) {
+            case ColorType.Red:
+                renderer.material.SetColor("_Color", Color.red);
+                break;
+            case ColorType.Green:
+                renderer.material.SetColor("_Color", Color.green);
+                break;
+            case ColorType.Blue:
+                renderer.material.SetColor("_Color", Color.blue);
+                break;
+        }
+    }
+}
---
+++ Assets/Source/Game/Events/cubepuzzle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class cubepuzzle : MonoBehaviour {
+    public EButton blueButton;
+    public EButton greenButton;
+    public EButton redButton;
+
+    public Color color = Color.yellow;
+
+    public  List<Transform> cubes = new List<Transform>();
+    private List<Color>    colors = new List<Color>();
+
+    void Start() {
+        Setup();
+    }
+
+    void Setup() {
+        for (int i = 0; i < cubes.Count; i++) {
+            Color color = new Color(Random01(), Random01(), 0);
+            Debug.Log(color);
+
+            Renderer rend = cubes[i].GetComponent<Renderer>();
+            rend.material.SetColor("_Color", color);
+        }
+    }
+
+    int Random01() {
+        Random rand = new Random();
+        
+        return (Random.Range(0.0f, 1f) >= 0.5f) ? 1 : 0;
+    }
+}
