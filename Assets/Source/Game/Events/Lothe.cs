eb3701ffd60012d914806c7d78b5d9e7a0efce90
muit <miguel_3c@hotmail.com>
2016-04-06 11:05:28 +0200

    [0.2] Second Prototype

---
+++ Assets/Source/Game/Events/Lothe.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+public class Lothe : MonoBehaviour
+{
+    public Transform head;
+    public float targetYawRotation;
+    public float speed = 3;
+
+    public void Rotate()
+    {
+        targetYawRotation -= 90;
+    }
+
+    void Update()
+    {
+        SetYaw(targetYawRotation, Application.isPlaying);
+    }
+
+    void SetYaw(float yaw, bool smooth)
+    {
+        if (!head) return;
+
+        Vector3 rotation = head.localEulerAngles;
+        if (smooth)
+        {
+            rotation.y = Mathf.LerpAngle(rotation.y, yaw, Time.deltaTime * speed);
+        }
+        else {
+            rotation.y = yaw;
+        }
+        head.localEulerAngles = rotation;
+    }
+
+}
